25e6c8b5c354c05aea96c0353262ad530111f331
Adam Farden <adam@farden.cz>
2015-02-10 19:32:57 +0100

    Define the device's kernel defconfig
    for inline kernel building we have to define
    kernel config for each device

--- aosp_d2203.mk
+++ aosp_d2203.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_KERNEL_CONFIG := aosp_yukon_flamingo_defconfig
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # Include others
