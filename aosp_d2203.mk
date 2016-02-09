829b8e8464249f8fd551983dc38a76c02ad58911
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-09 20:00:10 +0100

    device rework
    We split device from aosp_device so that it can be included
    easy in custom projects.
    
    New projects should create a new target named <project>_device
    which contains project specific configuration and options.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d2203.mk
+++ aosp_d2203.mk
@@ -14,48 +14,10 @@
 
 TARGET_KERNEL_CONFIG := aosp_yukon_flamingo_defconfig
 
-# Include others
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, device/sony/yukon/platform.mk)
-$(call inherit-product, vendor/sony/flamingo/flamingo-vendor.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/flamingo/defice.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/flamingo/overlay
-
-# Device etc
-PRODUCT_COPY_FILES += \
-    device/sony/flamingo/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/sony/flamingo/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
-    device/sony/flamingo/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    device/sony/flamingo/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/flamingo/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
-
-# IDC
-PRODUCT_COPY_FILES += \
-    device/sony/flamingo/rootdir/system/usr/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
-
-PRODUCT_PACKAGES += \
-    keystore.msm8226
-
-# Device Init
-PRODUCT_PACKAGES += \
-    fstab.flamingo \
-    init.recovery.flamingo \
-    init.flamingo \
-    ueventd.flamingo
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.flamingo
-
-# Simple PowerHAL
-PRODUCT_PACKAGES += \
-    power.flamingo
-
-# NFC config
-PRODUCT_PACKAGES += \
-    nfc_nci.pn54x.default
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Product attributes
 PRODUCT_NAME := aosp_d2203
@@ -63,11 +25,3 @@ PRODUCT_DEVICE := flamingo
 PRODUCT_MODEL := Xperia E3 (AOSP)
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
-
-PRODUCT_AAPT_CONFIG := large
-PRODUCT_AAPT_PREBUILT_DPI := hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240 \
-    ro.usb.pid_suffix=1BC
